5b919e6829e1d080e4930cd0fedf7231bc388e85
taomu010 <111723200+taomu010@users.noreply.github.com>
2022-08-23 12:08:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp']), math.floor(weather['high']), math.floor(weather['low']
+  return weather['weather'], math.floor(weather['temp']), math.floor(weather['high']), math.floor(weather['low'])
 
 
 #词霸每日一句
